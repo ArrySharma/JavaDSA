989f54e12892b699ed9642ff86dae4df39b86f8a
Arry <realarshdeepsharma@gmail.com>
2023-02-08 14:00:00 +0100

    feb java commit

---
+++ ConditionalStatements.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ConditionalStatements{
+    public static void main(String args[]){
+        int a = 1;
+        int b = 5;
+
+        if(a >= b){
+            System.out.println("A is greater than equal to b");
+        }
+        else{
+            System.out.println("B is greatert than A");
+        }
+    }
+}
---
+++ JavaBasics.java
@@ -0,0 +1,10 @@
+// first 2 lines are, Boiler plate code for java
+public class JavaBasics {                               // this should be same as the file name
+    public static void main(String args[]){             // our code will be within this main function
+        System.out.print("Hello world!\n");               // S should be capital, ; means this line has ended here
+        System.out.println("Hello world!");               // S should be capital, ; means this line has ended here, println is for next line(line next)
+        System.out.print("Hello world!\n");               // S should be capital, ; means this line has ended here, \n is for next line
+        
+
+    }
+}
